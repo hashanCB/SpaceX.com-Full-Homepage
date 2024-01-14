b2801da12d0232a5fa4c98cbd064b59f4da2a757
Hashan <hashan.chanaka96@gmail.com>
2024-01-14 03:58:57 +0000

    start spaceX Project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#355B3E",
+        secondary: "#F5DBC4",
+        dimgreenButton: "#029664",
+        dimgreenText: "#58745E",
+      },
+    backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+       // 'footer-texture': "url('/img/footer-texture.png')",
+       },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+       
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+ };
+ 
